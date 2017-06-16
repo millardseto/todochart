72dee8c88316ad26bcf7afdc6bca580987143d25
Millard Seto <millardseto@mac.com>
2017-06-16 14:48:34 -0700

    Beautify code

--- todo.js
+++ todo.js
@@ -1,69 +1,78 @@
-window.onload = function(){
+window.onload = function() {
 
-  function showChart(tasks){
-    var counts = [
-      {level:"Very Easy", count:0},
-      {level:"Easy", count:0},
-      {level:"Medium", count:0},
-      {level:"Hard", count:0},
-      {level:"Very Hard", count:0}
+  function showChart(tasks) {
+    var counts = [{
+        level: "Very Easy",
+        count: 0
+      },
+      {
+        level: "Easy",
+        count: 0
+      },
+      {
+        level: "Medium",
+        count: 0
+      },
+      {
+        level: "Hard",
+        count: 0
+      },
+      {
+        level: "Very Hard",
+        count: 0
+      }
     ]; // holds total counts for each level
 
     // loop through tasks and count each level
-    for (var i=0; i<tasks.length; i++){
+    for (var i = 0; i < tasks.length; i++) {
       counts[tasks[i].level].count++;
     }
 
     // build an array of arrays
     var rows = new Array();
-    for (var j=0; j<counts.length; j++){
+    for (var j = 0; j < counts.length; j++) {
       rows.push([counts[j].level, counts[j].count]);
     }
 
 
     // Load the Visualization API and the corechart package.
-          google.charts.load('current', {'packages':['corechart']});
+    google.charts.load('current', {
+      'packages': ['corechart']
+    });
 
-          // Set a callback to run when the Google Visualization API is loaded.
-          google.charts.setOnLoadCallback(drawChart);
+    // Set a callback to run when the Google Visualization API is loaded.
+    google.charts.setOnLoadCallback(drawChart);
 
-          // Callback that creates and populates a data table,
-          // instantiates the pie chart, passes in the data and
-          // draws it.
-          function drawChart() {
+    // Callback that creates and populates a data table,
+    // instantiates the pie chart, passes in the data and
+    // draws it.
+    function drawChart() {
 
-            // Create the data table.
-            var data = new google.visualization.DataTable();
-            data.addColumn('string', 'Difficulty');
-            data.addColumn('number', 'Level');
-            data.addRows(rows);
+      // Create the data table.
+      var data = new google.visualization.DataTable();
+      data.addColumn('string', 'Difficulty');
+      data.addColumn('number', 'Level');
+      data.addRows(rows);
 
-            /*
-            data.addRows([
-              ['Mushrooms', 3],
-              ['Onions', 1],
-              ['Olives', 1],
-              ['Zucchini', 1],
-              ['Pepperoni', 2]
-            ]);*/
+      // Set chart options
+      var options = {
+        'title': 'Percent of Difficulty',
+        'width': 400,
+        'height': 300
+      };
 
-            // Set chart options
-            var options = {'title':'Percent of Difficulty',
-                           'width':400,
-                           'height':300};
-
-            // Instantiate and draw our chart, passing in some options.
-            var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-            chart.draw(data, options);
+      // Instantiate and draw our chart, passing in some options.
+      var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+      chart.draw(data, options);
+    }
   }
-}
 
 
 
   /**
    * addTask - Adds a task to the TODO list and then shows it
    */
-  function addTask(){
+  function addTask() {
     // block the default behavior
     event.preventDefault();
 
@@ -75,7 +84,11 @@ window.onload = function(){
     var level = form["level"].value;
 
     // add new item to tasks
-    tasks.push({task:task, assignTo:assignTo, level:level});
+    tasks.push({
+      task: task,
+      assignTo: assignTo,
+      level: level
+    });
 
     // update list
     showList();
@@ -91,23 +104,22 @@ window.onload = function(){
    *
    * @return {type}  description
    */
-  function showList(){
+  function showList() {
     // clear existing content
     var ul = document.querySelector('ul');
     ul.innerText = "";
 
     // dictionary object for lookups - might need to be global var in the future.
-    var levels =
-    [
-        "Very Easy",
-        "Easy",
-        "Medium",
-        "Hard",
-        "Very Hard"
+    var levels = [
+      "Very Easy",
+      "Easy",
+      "Medium",
+      "Hard",
+      "Very Hard"
     ];
 
     // go through each task
-    for(var i=0; i<tasks.length; i++) {
+    for (var i = 0; i < tasks.length; i++) {
       // create list item and text
       var li = document.createElement("li");
       li.innerText = "Assigned To: " + tasks[i].assignTo + ", Level: " + levels[tasks[i].level] + ", Task: " + tasks[i].task;
